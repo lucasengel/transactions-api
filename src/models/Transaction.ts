70dbcfae6457b5464ef9262f622a31ada7f44477
Lucas Engel <lucasengel@gmail.com>
2020-09-15 15:50:43 -0400

    Add get and post transaction routes

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import Category from "./Category";
+
+@Entity("transactions")
 class Transaction {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
-
+  @Column("decimal")
   value: number;
 
+  @Column()
+  type: "income" | "outcome";
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from "typeorm";
+import AppError from "../errors/AppError";
+import Category from "../models/Category";
 
 import Transaction from "../models/Transaction";
+import TransactionsRepository from "../repositories/TransactionsRepository";
+
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type = "outcome",
+    category: categoryName = "Uncategorized",
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (!title || !value) throw new AppError("Missing parameters");
+
+    // Check for existing category
+    const existingCategory = await categoriesRepository.findOne({
+      where: {
+        title: categoryName,
+      },
+    });
+
+    const category =
+      existingCategory ||
+      categoriesRepository.create({
+        title: categoryName,
+      });
+
+    await categoriesRepository.save(category);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
